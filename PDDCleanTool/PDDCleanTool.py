de94f4cec1457ec2987bd4c5d93631aab15dfa05
xuwb1 <xuwb1@yonyou.com>
2024-05-29 11:07:20 +0800

    模板

---
+++ PDDCleanTool/PDDCleanTool.py
@@ -0,0 +1,18 @@
+def 订单表清洗(path):
+    print('清洗之后保存')
+
+
+def 售后表清洗(path):
+    print('清洗之后保存')
+
+
+def 佣金表清洗(path):
+    print('清洗之后保存')
+
+
+def 账单表清洗(path):
+    print('清洗之后保存')
+
+
+if __name__ == '__main__':
+    订单表清洗('');
