a99647c995727896155fe35b8834ef7d501d3df4
xuwb1 <xuwb1@yonyou.com>
2024-05-30 14:25:05 +0800

    订单全状态v1

---
+++ PDDCleanTool/PddOrderCost/PddOrderCost.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+
+from PDDCleanTool.common.CommonUtil import CommonUtil
+
+def 订单全状态表(path):
+    所有文件名列表 = os.listdir(path)
+    for 文件名 in 所有文件名列表:
+        if '订单表' in 文件名:
+            order_data = CommonUtil().读取表格(path,文件名);
+        elif '账单表' in 文件名:
+            bill_data =  CommonUtil().读取表格(path,文件名);
+
+    bill_data.rename(columns={'商户订单号': '订单号'}, inplace=True)
+    #结算状态为交易收入的
+    settle_status_data = bill_data[bill_data['账务类型'].isin(['交易收入'])]
+    settle_status_data = settle_status_data[['订单号','账务类型','发生时间']]
+    #结算金额
+    # bill_data[['订单号','收入金额']]
+
+    # 订单-账单状态-账单金额   连接
+    order_bill_data = pd.merge(order_data, settle_status_data, how='left', on='订单号')
+
+    有售后 = order_bill_data[order_bill_data['售后状态'].isin(['退款成功'])]
+    无售后 = order_bill_data[~(order_bill_data['售后状态'].isin(['退款成功']))]
+
+    回退 = 有售后[有售后['账务类型'].isin(['交易收入'])]
+    已退 = 有售后[~(有售后['账务类型'].isin(['交易收入']))]
+    已回 = 无售后[无售后['账务类型'].isin(['交易收入'])]
+    待回 = 无售后[~(无售后['账务类型'].isin(['交易收入']))]
+
+    回退['回退状态'] = '回退'
+    已退['回退状态'] = '已退'
+    已回['回退状态'] = '已回'
+    待回['回退状态'] = '待回'
+    合并数据 = pd.concat([回退, 已退, 已回, 待回])
+    pd.DataFrame(合并数据).to_csv('aa.csv')
+    print()
+
+if __name__ == '__main__':
+    订单全状态表('C:\\Users\\xwb\\Desktop\\拼多多-MVAV鞋服工厂店\\拼多多-MVAV鞋服工厂店\\2、清洗后');
--- PDDCleanTool/common/CommonUtil.py
+++ PDDCleanTool/common/CommonUtil.py
@@ -1,16 +1,20 @@
 import pandas as pd
 
 class CommonUtil():
-    def 读取表格(path,name):
+    ##统一读取文件，解决（文件编码、制表符等）
+    def 读取表格(self,path,name):
         # 路径+文件名
         文件位置 = path+'\\'+name
+        data = pd.DataFrame()
         if 'xlsx' in name:
             try:
-                return pd.read_excel(文件位置,dtype=str);
+                data = pd.read_excel(文件位置,dtype=str).rename(columns=lambda x: x.strip())
             except(UnicodeDecodeError):
-                return pd.read_excel(文件位置,dtype=str, encoding='GB18030')
+                data = pd.read_excel(文件位置,dtype=str, encoding='GB18030').rename(columns=lambda x: x.strip())
         elif 'csv' in name:
             try:
-                return pd.read_csv(文件位置,dtype=str);
+                data = pd.read_csv(文件位置,dtype=str).rename(columns=lambda x: x.strip())
             except(UnicodeDecodeError):
-                return pd.read_csv(文件位置,dtype=str, encoding='GB18030')
+                data = pd.read_csv(文件位置,dtype=str, encoding='GB18030').rename(columns=lambda x: x.strip())
+        data = data.replace('\t', '', regex=True).replace('\n', '', regex=True)
+        return data
